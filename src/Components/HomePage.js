a0bbf6d7926ffe94a3a4bf05407cba620a607601
Alaudhin936 <160020112+Alaudhin936@users.noreply.github.com>
2024-06-01 21:57:35 +0530

    Update HomePage.js

--- src/Components/HomePage.js
+++ src/Components/HomePage.js
@@ -1,86 +1,8 @@
 export default function HomePage(){
     return(
         <>
-    <h1>Home
-    ible. Provide a valid, navigable address as the href value. If you cannot provide an href, but still need the element to resemble a link, use a button and change it with appropriate styles. Learn more: https://github.com/jsx-eslint/eslint-plugin-jsx-a11y/blob/HEAD/docs/rules/anchor-is-valid.md  jsx-a11y/anchor-is-valid    
-  Line 43:19:  img elements must have an alt prop, either with meaningful text, or an empty string for decorative images                                          
-                                                                                 
-                                                                                 
-                                                     jsx-a11y/alt-text
-  Line 76:15:  img elements must have an alt prop, either with meaningful text, or an empty string for decorative images                                          
-                                                                                 
-                                                                                 
-                                                     jsx-a11y/alt-text
-
-src\Components\Service.js
-  Line 69:1:  img elements must have an alt prop, either with meaningful text, or an empty string for decorative images  jsx-a11y/alt-text
-
-Search for the keywords to learn more about each warning.
-To ignore, add // eslint-disable-next-line to the line before.
-
-WARNING in [eslint]
-src\App.js
-  Line 29:15:  img elements must have an alt prop, either with meaningful text, or an empty string for decorative images                                          
-                                                                                 
-                                                                                 
-                                                     jsx-a11y/alt-text
-  Line 35:21:  The href attribute is required for an anchor to be keyboard accessible. Provide a valid, navigable address as the href value. If you cannot provide an href, but still need the element to resemble a link, use a button and change it with appropriate styles. Learn more: https://github.com/jsx-eslint/eslint-plugin-jsx-a11y/blob/HEAD/docs/rules/anchor-is-valid.md  jsx-a11y/anchor-is-valid    
-  Line 43:19:  img elements must have an alt prop, either with meaningful text, or an empty string for decorative images                                          
-                                                                                 
-                                                                                 
-                                                     jsx-a11y/alt-text
-  Line 76:15:  img elements must have an alt prop, either with meaningful text, or an empty string for decorative images                                          
-                                                                                 
-                                                                                 
-                                                     jsx-a11y/alt-text
-
-src\Components\Service.js
-  Line 69:1:  img elements must have an alt prop, either with meaningful text, or
-Compiled with warnings.
-
-[eslint]
-src\App.js
-  Line 29:15:  img elements must have an alt prop, either with meaningful text, or an empty string for decorative images                                          
-                                                                                 
-                                                                                 
-                                                     jsx-a11y/alt-text
-  Line 35:21:  The href attribute is required for an anchor to be keyboard accessible. Provide a valid, navigable address as the href value. If you cannot provide an href, but still need the element to resemble a link, use a button and change it with appropriate styles. Learn more: https://github.com/jsx-eslint/eslint-plugin-jsx-a11y/blob/HEAD/docs/rules/anchor-is-valid.md  jsx-a11y/anchor-is-valid    
-  Line 43:19:  img elements must have an alt prop, either with meaningful text, or an empty string for decorative images                                          
-                                                                                 
-                                                                                 
-                                                     jsx-a11y/alt-text
-  Line 76:15:  img elements must have an alt prop, either with meaningful text, or an empty string for decorative images                                          
-                                                                                 
-                                                                                 
-                                                     jsx-a11y/alt-text
-
-src\Components\Service.js
-  Line 69:1:  img elements must have an alt prop, either with meaningful text, or an empty string for decorative images  jsx-a11y/alt-text
-
-Search for the keywords to learn more about each warning.
-To ignore, add // eslint-disable-next-line to the line before.
-
-WARNING in [eslint] 
-src\App.js
-  Line 29:15:  img elements must have an alt prop, either with meaningful text, or an empty string for decorative images                                          
-                                                                                 
-                                                                                 
-                                                     jsx-a11y/alt-text
-  Line 35:21:  The href attribute is required for an anchor to be keyboard accessible. Provide a valid, navigable address as the href value. If you cannot provide an href, but still need the element to resemble a link, use a button and change it with appropriate styles. Learn more: https://github.com/jsx-eslint/eslint-plugin-jsx-a11y/blob/HEAD/docs/rules/anchor-is-valid.md  jsx-a11y/anchor-is-valid    
-  Line 43:19:  img elements must have an alt prop, either with meaningful text, or an empty string for decorative images                                          
-                                                                                 
-                                                                                 
-                                                     jsx-a11y/alt-text
-  Line 76:15:  img elements must have an alt prop, either with meaningful text, or an empty string for decorative images                                          
-                                                                                 
-                                                                                 
-                                                     jsx-a11y/alt-text
-
-src\Components\Service.js
-  Line 69:1:  img elements must have an alt prop, either with meaningful text, or an empty string for decorative images  jsx-a11y/alt-text
-
-webpack compiled with 1 warning
- 
+    <h1>
+ Home Page
     </h1>
         </>
     )
